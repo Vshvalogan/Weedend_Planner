b1446ef0afed1e394ecc5520a5e27185186ff979
vshvalogan <loganathan.vshvapoorani@gmail.com>
2025-10-08 10:12:55 +0800

    create states to fetch form api

--- src/App.jsx
+++ src/App.jsx
@@ -4,47 +4,29 @@ import NavBar from "./components/NavBar";
 import Homepage from "./pages/Homepage";
 import PlacePage from "./pages/PlacePage";
 import PlaceDetailsPage from "./pages/PlaceDetailsPage";
-import SavePlacesPage from "./pages/SavePlacesPage";
 
 function App() {
-  const [places, setPlaces] = useState([]);      // search results
-  const [savedPlaces, setSavedPlaces] = useState([]);  // saved list
-
-  const savePlace = (place) => {
-    setSavedPlaces([...savedPlaces, place]);
-  };
-
-  const deletePlace = (id) => {
-    const newList = savedPlaces.filter((p) => p.id !== id);
-    setSavedPlaces(newList);
-  };
+  const [places, setPlaces] = useState([]);
+  const [mapImage, setMapImage] = useState(null);
 
   return (
     <>
       <NavBar />
-      {/* <Routes>
-        <Route path="/" element={<Homepage setPlaces={setPlaces} />} />
-        <Route path="/places" element={<PlacePage places={places} />} />
-        <Route
-          path="/places/:id"
-          element={<PlaceDetailsPage savePlace={savePlace} />}
-        />
-        <Route
-          path="/places/savedplaces"
-          element={<SavePlacesPage savedPlaces={savedPlaces} deletePlace={deletePlace} />}
-        />
-      </Routes> */}
       <Routes>
-        <Route path="/" element={<Homepage setPlaces={setPlaces} />}>
-          <Route path="places" element={<PlacePage places={places} />}>
-            <Route path=":id" element={<PlaceDetailsPage savePlace={savePlace} />} />
+        <Route
+          path="/"
+          element={<Homepage setPlaces={setPlaces} setMapImage={setMapImage} />}
+        >
+          <Route
+            path="places"
+            element={<PlacePage places={places} mapImage={mapImage} />}
+          >
+            <Route
+              path=":id"
+              element={<PlaceDetailsPage places={places} />}
+            />
           </Route>
         </Route>
-
-        <Route
-          path="/places/savedplaces"
-          element={<SavePlacesPage savedPlaces={savedPlaces} deletePlace={deletePlace} />}
-        />
       </Routes>
     </>
   );
